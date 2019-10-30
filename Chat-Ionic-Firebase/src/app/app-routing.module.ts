efc1ea79e4f301206793cc0c7c075cdf2ab5b53c
elenadri <adrianamourabueno@gmail.com>
2019-10-30 19:53:21 -0300

    cadastro de usuario

--- Chat-Ionic-Firebase/src/app/app-routing.module.ts
+++ Chat-Ionic-Firebase/src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule),canActivate:[AuthGuard]},
   { path: 'login', loadChildren: './componentes/login/login.module#LoginPageModule', canActivate : [NoLoginGuard] },
+  { path: 'registro', loadChildren: './componentes/registro/registro.module#RegistroPageModule' },
 
 ];
 
--- Chat-Ionic-Firebase/src/app/service/auth.service.ts
+++ Chat-Ionic-Firebase/src/app/service/auth.service.ts
@@ -1,28 +1,25 @@
 import { Injectable } from '@angular/core';
-import { AngularFireAuth } from '@angular/fire/auth';
-import { Router } from '@angular/router';
-
-
+import { AngularFireAuth } from "@angular/fire/auth";
+import { promise } from 'protractor';
+import { Router } from "@angular/router";
+import { AngularFirestore } from "@angular/fire/firestore";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor(private  AFauth : AngularFireAuth,  private router : Router) {}
+  constructor(private AFauth : AngularFireAuth, private router : Router, private db : AngularFirestore) { }
 
-  login( email: string, password:string){
+  login(email:string, password:string){
 
-    return new Promise((resolve, rejected)=> {
-      this.AFauth.auth.signInWithEmailAndPassword(email,password).then(res => {
-       
-          resolve(res);
-
+    return new Promise((resolve, rejected) =>{
+      this.AFauth.auth.signInWithEmailAndPassword(email, password).then(user => {
+        resolve(user);
       }).catch(err => rejected(err));
-
     });
 
-
+   
   }
 
   logout(){
@@ -30,4 +27,23 @@ export class AuthService {
       this.router.navigate(['/login']);
     })
   }
+
+  register(email : string, password : string, name : string){
+
+    return new Promise ((resolve, reject) => {
+      this.AFauth.auth.createUserWithEmailAndPassword(email, password).then( res =>{
+          // console.log(res.user.uid);
+        const uid = res.user.uid;
+          this.db.collection('users').doc(uid).set({
+            name : name,
+            uid : uid
+          })
+        
+        resolve(res)
+      }).catch( err => reject(err))
+    })
+    
+
+  }
+
 }
